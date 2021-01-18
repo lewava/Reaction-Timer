4b63d8045393e5ce27ce5ed4d299598af921d699
Leon Wass Vallin <leon.wass.vallin@gmail.com>
2021-01-18 15:37:26 +0100

    Project finished

---
+++ src/components/Results.vue
@@ -0,0 +1,29 @@
+<template>
+  <p>Reaction Time - {{ score }}ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props : ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) this.rank = 'Amazing!'
+        else if (this.score < 400) this.rank = 'Good'
+        else if (this.score < 500) this.rank = 'Ok'
+        else this.rank = 'Slow'
+    }
+}
+</script>
+
+<style>
+    .rank {
+       font-size: 1.4em; 
+       color: #0faf87;
+       font-weight: bold;
+    }
+</style>
